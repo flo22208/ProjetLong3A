0cd2f7268166785b6446efe73ff9e59f7e6d2873
pekatour <pekatour.64@gmail.com>
2026-02-23 10:41:43 +0100

    do not clamp hihi

--- test_du_R.py
+++ test_du_R.py
@@ -31,7 +31,7 @@ def test_rusinkiewicz_function():
 
         L, V, n, _, t = rusinkiewicz_to_LV(theta_h, phi_h, theta_d, phi_d)
 
-        phi_d2, theta_d2, theta_h2 = db.rusinkiewicz_angles(np.array([n]), np.array([L]), np.array([V]))
+        phi_d2, theta_d2, theta_h2 = db.rusinkiewicz_angles(np.array([n]), np.array([L]), np.array([V]), False)
         
         phi_d2 = phi_d2[0]
         theta_d2 = theta_d2[0]
