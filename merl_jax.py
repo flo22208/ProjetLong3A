aa0d4e0a4970746d178f6b8fa72d113e27760965
yannis rosseel <yannis.rosseel1@gmail.com>
2026-03-04 18:52:15 +0100

    jax sur merl

---
+++ merl_jax.py
@@ -0,0 +1,69 @@
+import time
+from matplotlib import pyplot as plt
+from merlDB import database as db
+import jax.numpy as jnp
+from render_jax import optimize_params
+
+## load BRDF from MERL database
+dbuilder = db.DBuilder(interp_method="linear",db_path='merlDB/db/brdfs/')
+ldb = dbuilder.list_db()
+mat = "dark-blue-paint"
+dbuilder.load_mat(mat)
+print("Loaded " + mat)
+brdf = dbuilder.brdf_function(mat)
+name = mat
+
+## Create table of all angles
+RES_THETA_H = 90
+RES_THETA_D = 90
+RES_PHI_D = 180
+
+MAX_THETA_H = 90
+MAX_THETA_D = 90
+MAX_PHI_D = 180
+
+theta_hs = jnp.deg2rad(jnp.linspace(0, RES_THETA_H, MAX_THETA_H))
+theta_ds = jnp.deg2rad(jnp.linspace(0, RES_THETA_D, MAX_THETA_D))
+phi_ds = jnp.deg2rad(jnp.linspace(0, RES_PHI_D, MAX_PHI_D))
+
+# Create full meshgrid
+TH, TD, PH = jnp.meshgrid(theta_hs, theta_ds, phi_ds, indexing="ij")
+angles = jnp.stack([PH, TD, TH], axis=-1).reshape(-1, 3)
+
+rgbs = brdf(angles)
+## Resize to MAX_THETA_H, MAX_THETA_D, MAX_PHI_D, 3
+brdf_target = rgbs.reshape(MAX_THETA_H, MAX_THETA_D, MAX_PHI_D, 3)
+brdf_target = jnp.moveaxis(brdf_target, -1, 0)
+brdf_target = brdf_target / (1.0 + brdf_target) # tone mapping
+
+print("Début de l'optimisation")
+t1 = time.time()
+params, _, loss_finale, loss_hist, pred_finale = optimize_params(TH, TD, PH, brdf_target, steps=1000, lr=1e-2)
+t2 = time.time()
+print(f"Finished in {(t2-t1):3f} s")
+print(f"loss finale : {loss_finale:6f}")
+plt.semilogy(loss_hist)
+plt.grid(True)
+plt.xlabel("Iteration")
+plt.ylabel("Loss")
+plt.title("BRDF fitting loss")
+plt.show()
+params_full = {
+    "baseColor": params["baseColor"],
+    "metallic": params["metallic"],
+    "subsurface": params["subsurface"],
+    "specular": params["specular"],
+    "roughness": params["roughness"],
+    "specularTint" : 0.5,
+    "anisotropic" : 0.0,
+    "sheen": params["sheen"],
+    "sheenTint" : 0.0,
+    "clearcoat": params["clearcoat"],
+    "clearcoatGloss" : 0.0,
+}
+pred_finale = jnp.moveaxis(pred_finale,0,-1)
+print("MERL mean:", brdf_target.mean(axis=(1,2,3)))
+print("Disney mean:", pred_finale.mean(axis=(1,2,3)))
+jnp.savez(f"brdfs_disney/brdf_0.npz",
+        params=params_full,
+        brdf=pred_finale)
--- render_jax.py
+++ render_jax.py
@@ -4,40 +4,22 @@ import jax
 import optax
 import numpy as np
 import jax.numpy as jnp
-from randomDisneyBRDFSaver import MAX_PHI_D, MAX_THETA_D, MAX_THETA_H, RES_PHI_D, RES_THETA_D, RES_THETA_H
-import utilsBRDFDisney
+import numpyBRDF
+from randomBRDFSaver import MAX_PHI_D, MAX_THETA_D, MAX_THETA_H, RES_PHI_D, RES_THETA_D, RES_THETA_H
 import utils_jax
-import sys
-sys.path.append("..")
-import rendering
+
 BRDF_jitted = jax.jit(utils_jax.BRDF_jax)
 
 def init_param():
-    scale = 0.5
-    baseColor = jnp.array(scale * np.random.randn(3))
-    metallic = jnp.array(scale * np.random.randn())
-    subsurface = jnp.array(0.0)
-    specular = jnp.array(scale * np.random.randn())
-    roughness = jnp.array(scale * np.random.randn())
-    specularTint = jnp.array(0.0)
-    anisotropic = jnp.array(0.0)
-    sheen = jnp.array(scale * np.random.randn())
-    sheenTint = jnp.array(0.0)
-    clearcoat = jnp.array(scale * np.random.randn())
-    clearcoatGloss = jnp.array(0.0)
-    
+    scale = 0.5    
     material_params = {
-        "baseColor": baseColor,
-        "metallic": metallic,
-        #"subsurface": subsurface,
-        "specular": specular,
-        "roughness": roughness,
-        #"specularTint": specularTint,
-        #"anisotropic": anisotropic,
-        "sheen": sheen,
-        #"sheenTint": sheenTint,
-        "clearcoat": clearcoat,
-        #"clearcoatGloss": clearcoatGloss,
+        "baseColor": jnp.array(scale * np.random.randn(3)),
+        "metallic": jnp.array(scale * np.random.randn()),
+        "subsurface": jnp.array(scale * np.random.randn()),
+        "specular": jnp.array(scale * np.random.randn()),
+        "roughness": jnp.array(scale * np.random.randn()),
+        "sheen": jnp.array(scale * np.random.randn()),
+        "clearcoat": jnp.array(scale * np.random.randn()),
     }
     return material_params
     
@@ -48,18 +30,17 @@ def loss_fn(params, L, V, N, X, Y, brdf_target):
     roughness = jax.nn.sigmoid(params["roughness"])
     sheen     = jax.nn.sigmoid(params["sheen"])
     clearcoat = jax.nn.sigmoid(params["clearcoat"])
+    subsurface = jax.nn.sigmoid(params["subsurface"])
     pred = BRDF_jitted(L, V, N, X, Y, baseColor=baseColor,
     metallic=metallic,
-    #params["subsurface"],
+    subsurface=0,#subsurface,
     specular=specular,
+    specularTint=0.5,
     roughness=roughness,
-    #params["specularTint"],
-    #params["anisotropic"],
     sheen=sheen,
     sheenTint=0,
     clearcoat=clearcoat,
     clearcoatGloss=0)
-    #params["clearcoatGloss"])
     pred = pred / (1.0 + pred) # tone mapping
     # eps = 1e-6
     # loss = jnp.mean((pred - brdf_target)**2) # MSE 2)
@@ -70,23 +51,7 @@ def loss_fn(params, L, V, N, X, Y, brdf_target):
     # loss = jnp.mean((jnp.log(pred+eps) - jnp.log(brdf_target+eps))**2) # Log-MSE 5)
     loss = optax.huber_loss(pred, brdf_target, delta=0.01).mean() # Huber Loss 1)
     # loss = optax.log_cosh(pred, brdf_target).mean() # Log-Cos 3)
-    return loss
-
-def params_to_vec(params):
-    return jnp.concatenate([
-        params["baseColor"].reshape(-1),   # 3 valeurs
-        jnp.array([params["metallic"]]),
-        #jnp.array([params["subsurface"]]),
-        jnp.array([params["specular"]]),
-        jnp.array([params["roughness"]]),
-        #jnp.array([params["specularTint"]]),
-        #jnp.array([params["anisotropic"]]),
-        jnp.array([params["sheen"]]),
-        #jnp.array([params["sheenTint"]]),
-        jnp.array([params["clearcoat"]]),
-        #jnp.array([params["clearcoatGloss"]]),
-    ])
-    
+    return loss    
 
 def optimize_params(TH, TD, PH, brdf_target, steps=1000, lr=1e-2): # 2000 5e-3
     params = init_param()
@@ -103,26 +68,24 @@ def optimize_params(TH, TD, PH, brdf_target, steps=1000, lr=1e-2): # 2000 5e-3
         updates, opt_state = optimizer.update(grads, opt_state)
         params = optax.apply_updates(params, updates)
         return params, opt_state, loss
+    
     loss_hist = []
     # 3) Boucle d’optimisation
-    for step_i in range(steps):
+    for _ in range(steps):
         params, opt_state, loss = step(params, opt_state,L, V, N, X, Y, brdf_target)
         loss_hist.append(loss)
-        # if step_i % 10 == 0:
-            # print(f"step {step_i}, loss = {float(loss):.6f}")
+        
     params = {k: jax.nn.sigmoid(v) for k, v in params.items()}
     pred_finale = BRDF_jitted(L, V, N, X, Y, baseColor=params["baseColor"],
     metallic=params["metallic"],
-    #params["subsurface"],
+    subsurface=0,#params["subsurface"],
     specular=params["specular"],
+    specularTint=0.5,
     roughness=params["roughness"],
-    #params["specularTint"],
-    #params["anisotropic"],
     sheen=params["sheen"],
     sheenTint=0,
     clearcoat=params["clearcoat"],
     clearcoatGloss=0)
-    #params["clearcoatGloss"])
 
     return params, params_init_jax, loss, loss_hist, pred_finale
 
@@ -144,23 +107,15 @@ def test():
     phi_ds = np.deg2rad(np.linspace(0, RES_PHI_D, MAX_PHI_D))
     TH, TD, PH = jnp.meshgrid(theta_hs, theta_ds, phi_ds, indexing="ij")
     L, V, N, X, Y = utils_jax.rusinkiewicz_to_LV_jax(TH, 0.0, TD, PH)
-    out_jax = utils_jax.BRDF_jax(L, V, N, X, Y, baseColor,
-    metallic,
-    subsurface,
-    specular,
-    roughness,
-    specularTint,
-    anisotropic,
-    sheen,
-    sheenTint,
-    clearcoat,
-    clearcoatGloss)
+    out_jax = utils_jax.BRDF_jax(L, V, N, X, Y, baseColor,metallic,subsurface,specular,
+    roughness,specularTint,anisotropic,
+    sheen,sheenTint,clearcoat,clearcoatGloss)
     brdf = np.zeros((MAX_THETA_H, MAX_THETA_D, MAX_PHI_D, 3), dtype=np.float32)
     for hi in range(MAX_THETA_H):
         for di in range(MAX_THETA_D):
             for pi in range(MAX_PHI_D):
                
-                vals = utilsBRDFDisney.BRDF(
+                vals = numpyBRDF.BRDF(
                     L[...,hi,di,pi], V[...,hi,di,pi], N[...,hi,di,pi], X[...,hi,di,pi], Y[...,hi,di,pi],
                     baseColor, metallic, subsurface,
                     specular, roughness, specularTint,
@@ -175,10 +130,17 @@ def test():
     rel_err = abs_err / (jnp.abs(brdf) + 1e-8)
     print(f"Erreur absolue : {np.mean(abs_err)}")
     print(f"Erreur relative : {np.mean(rel_err)}")
+    
+def affiche(x):
+    x = np.array(x)
+    if x.ndim == 0:
+        return f"{float(x):.4f}"
+    else:
+        return "[" + ", ".join(f"{float(v):.4f}" for v in x) + "]"
 
 if __name__ == "__main__":
     # test()
-    data = np.load("brdfs_disney/brdf_2.npz", allow_pickle=True)
+    data = np.load("brdfs_disney/brdf_5.npz", allow_pickle=True)
     brdf_target = jnp.array(data["brdf"])  # (H,D,P,3)
     params_init = data["params"].item()
     brdf_target = jnp.moveaxis(brdf_target,-1,0)
@@ -191,19 +153,24 @@ if __name__ == "__main__":
     # Create full meshgrid
     TH, TD, PH = jnp.meshgrid(theta_hs, theta_ds, phi_ds, indexing="ij")
     t1 = time.time()
-    params, params_init_jax, loss_finale, loss_hist, pred_finale = optimize_params(TH, TD, PH, brdf_target)
+    params, params_init_jax, loss_finale, loss_hist, pred_finale = optimize_params(TH, TD, PH, brdf_target, steps=1000, lr=5e-2)
     t2 = time.time()
-    print("Finished in", t2 -t1, "s")
+    print(f"Finished in {(t2-t1):3f} s")
     print("\n=== Parameters comparison ===")
-    print(f"Loss finale : {loss_finale}")
+    abs_params = {}
     for key in params.keys():
-        print(f"{key:12s} | Original: {params_init[key]}   →   Prédiction: {params[key]}")
-    renderer = rendering.Renderer(size=1000, save_path='rendered_frames/',nb_spheres=1,nb_tours=10,gamma=2.222)
-    renderer.render(pred_finale, nb_images=10,name="pred",MP=8)
-    renderer.render(brdf_target, nb_images=10,name="brdf_target",MP=8)
-    plt.plot(loss_hist)
+        original = params_init[key]
+        prediction = params[key]
+        abs_params[key] = np.mean(np.abs(original - prediction))
+        print(f"{key:12s} | Original: {affiche(original)}   →   Prédiction: {affiche(prediction)}")
+    plt.semilogy(loss_hist)
     plt.grid(True)
     plt.xlabel("Iteration")
     plt.ylabel("Loss")
     plt.title("BRDF fitting loss")
     plt.show()
+    np.savez(f"brdfs_disney/brdf_pred.npz",
+             params=params,
+             brdf=pred_finale)
+    abs_global = np.mean([v for v in abs_params.values()])
+    print(f"\nDifférence absolue globale paramètres : {abs_global:.4f}")
